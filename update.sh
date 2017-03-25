bde55e0f6d2e77739a7a4f3bc56f46c0ed88d824
muniz <linuxclimer@gmail.com>
2017-03-25 20:22:40 -0300

    luciana local

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# data = 23/03/2017
-# autor= MUniz SYSADM
-echo "atualizando sistemas linux.."
+#data=08/02/2017
+#author= Muniz SYSADMIN
+#Função=Atualizar sistemas LINUX
+#E-mail = <mcarlos370@gmail.com>
+#Version= 1.0 <08-02-2017>
+echo "atualizando sistema..."
 sleep 1
-echo " "
-echo " "
-echo -n ". . ."
-sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
-
-
+echo -n .
+sleep 1
+echo -n .
+sleep 1
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
